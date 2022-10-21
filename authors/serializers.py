e4451d21a7e5d9ce62818ed87d04df9e7d33d728
Qasim Khawaja <q.khawaja@yahoo.com>
2022-10-20 18:50:56 -0600

    feat: authors with the correct fields
    Co-authored-by: mharis2 <mharis2@users.noreply.github.com>
    Co-authored-by: Nat <NatDev7@users.noreply.github.com>

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Author
-
+import os
 class AuthorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    type = serializers.SerializerMethodField('get_type')
     display_name = serializers.CharField(max_length=100)
-    email = serializers.EmailField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+    github = serializers.CharField(max_length=100, required=False)
+    host = serializers.SerializerMethodField('get_host')
+    id = serializers.SerializerMethodField('get_id')
+    url = serializers.SerializerMethodField('get_id')
+    profileImage = serializers.SerializerMethodField('get_img')
 
     def create(self, validated_data):
         return Author.objects.create(**validated_data)
@@ -14,3 +16,18 @@ class AuthorSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.display_name = validated_data.get('display_name', instance.display_name)
         instance.github = validated_data.get('github', instance.github)
+    
+    def get_type(self, model : Author):
+        return 'authors'
+    
+    def get_host(self, model : Author):
+        return os.environ.get('HOSTNAME', 'http://127.0.0.1:5454')
+
+    def get_id(self, model : Author):
+        return f"{self.get_host(model)}/{self.get_type(model)}/{int(model.id)}"
+    
+    def get_img(self, model: Author):
+        if model.profileImage == "":
+            return "https://i.imgur.com/k7XVwpB.jpeg"
+        else:
+            return model.profileImage
--- authors/tests.py
+++ authors/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Author
 
-# Create your tests here.
+class AuthorTestCase(TestCase):
+    def setUp(self):
+        Author.objects.create(display_name = 'test', email = 'test1', password = 'test1', username = 'test1')
+        Author.objects.create(display_name = 'test2', email = 'test2', password = 'test2', username = 'test2')
+
+    def test_author(self):
+        test1 = Author.objects.get(display_name = 'test')
+        test2 = Author.objects.get(display_name = 'test2')
+        self.assertEqual(test1.display_name, 'test')
+        self.assertEqual(test2.display_name, 'test2')
+        
+    def test_duplicate_fail(self):
+        try:
+            test1 = Author.objects.create(display_name = 'test', email = 'test1', password = 'test1', username = 'test1')
+            test2 = Author.objects.create(display_name = 'test', email = 'test1', password = 'test1', username = 'test1')
+        except:
+            pass
+        else:
+            self.fail("Duplicate author created")
+    
+    def test_get_all(self):
+        all_authors = Author.objects.all()
+        self.assertEqual(len(all_authors), 2)
