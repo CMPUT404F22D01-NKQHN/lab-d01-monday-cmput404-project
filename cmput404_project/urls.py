a4a5476fb866bd7dda78cb68d708c3ab3e0c6483
Qasim Khawaja <q.khawaja@yahoo.com>
2022-10-24 17:02:00 -0600

    feat: onboard to open api for api docs
    * TODO: update the views to add serializers and example input

--- cmput404_project/urls.py
+++ cmput404_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authors/', include('authors.urls')),
     path('friends/', include('friends.urls')),
     path('posts/', include('posts.urls')),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
