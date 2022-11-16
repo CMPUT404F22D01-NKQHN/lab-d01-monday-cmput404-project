3c6c7ee2cd59b02deac2cccb135e83f2faa96ec0
Qasim Khawaja <q.khawaja@yahoo.com>
2022-11-15 20:43:03 -0700

    fix: disable test runner

--- cmput404_project/settings.py
+++ cmput404_project/settings.py
@@ -136,4 +136,4 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = '/media/'
-django_on_heroku.settings(locals())
+django_on_heroku.settings(locals(), test_runner=False)
